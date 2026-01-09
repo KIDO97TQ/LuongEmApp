ae09bcffbb5937a951d25aa2ac02d2083766588e
Kido_Nguyen <duythanh20101997@gmil.com>
2026-01-10 06:57:07 +0700

    Full upload46

--- FistWeb/obj/Debug/net8.0/FistWeb.AssemblyInfo.cs
+++ FistWeb/obj/Debug/net8.0/FistWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FistWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50ff9a08dff8e0a16130b1b120ea3a061f31751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186153a6f865b23d71c3fe27f1b5feed6edcf56e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FistWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FistWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
